1a0302ab56ae51e0327f1fc7d08c07d282fab195
earthgta2010 <earthgta2010@gmail.com>
2019-02-24 15:29:45 +0700

    'Foruth_commit'

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
 import { Post } from '../post';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
@@ -11,23 +12,93 @@ import { Post } from '../post';
 export class PostComponent implements OnInit {
 
   posts: Post[] = [];
+  form: FormGroup;
+
   constructor(
+    private fb: FormBuilder,
     private titleService: Title,
     private http: HttpClient
-  ) { 
+  ) {
     this.titleService.setTitle('Posts');
   }
 
   ngOnInit() {
-    const obj$ = this.http.get('https://jsonplaceholder.typicode.com/posts');
+    const obj$ = this.http.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
     obj$.subscribe({
-      next: (response: any[]) =>{
-        this.posts = response.slice(0,5).map((res) => {
+      next: (response: any[]) => {
+        this.posts = response.slice(0, 5).map((res) => {
           return new Post(res.id, res.title, res.body);
         });
         console.log(this.posts);
       }
     })
+    this.form = this.fb.group({
+      id: [''],
+      title: [''],
+      body: [''],
+    })
+  }
+
+  onSubmit(form: FormGroup) {
+    const value = form.value;
+    if (value.id) {
+      //update
+      const obj$ = this.http.put(
+        'https://jsonplaceholder.typicode.com/posts/' + value.id,
+        value
+      );
+      obj$.subscribe({
+        next: (response: any) => {
+          const post = new Post(response.id, response.title, response.body);
+          const index = this.posts.findIndex((p) => { return p.id === post.id });
+          this.posts[index] = post;
+        }
+      })
+
+    } else {
+      //create
+      const obj$ = this.http.post(
+        'https://jsonplaceholder.typicode.com/posts',
+        value
+      );
+      obj$.subscribe({
+        next: (response: any) => {
+          console.log(response);
+          const post = new Post(response.id, response.title, response.body);
+          // this.posts = [post, ...this.posts]; This create new array
+          this.posts.unshift(post); //This use origin array
+          this.form.reset();
+        }
+      })
+    }
+
+  }
+
+  onClick(post: Post) {
+    this.form.patchValue({
+      id: post.id,
+      title: post.title,
+      body: post.body
+    })
+  }
+
+  onDelete(post: Post) {
+    const con = confirm('Are you sure?');
+    if (con) {
+      const obj$ = this.http.delete(
+        'https://jsonplaceholder.typicode.com/posts/' + post.id
+      );
+
+      obj$.subscribe({
+        next: () => {
+          // const index = this.posts.findIndex((p) => { return p.id === post.id });
+          // this.posts.splice(index, 1);
+          this.posts = this.posts.filter(p => p.id !== post.id);
+        }
+      });
+    }
   }
 
 }
